ddd530f6aea85543c3dd3ed2ebdd13c82f63c75f
Ali Al-Bahrani <alibahrani@mac.com>
2018-08-27 17:07:07 -0400

    leaving 5:06 pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router'
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,9 @@ import { RouterModule } from '@angular/router'
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent }, 
       { path:'', redirectTo: 'welcome', pathMatch: 'full'},
       { path: '**', redirectTo:'welcom', pathMatch: 'full'}
